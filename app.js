3c35743fff6b7abb9d8568da35332e26ccf423a9
KevinOrellana00151220 <62663311+KevinOrellana00151220@users.noreply.github.com>
2024-09-26 15:06:25 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const AuthMiddleware = require('./middlewares/Auth');
+
+const apiRouter = require('./routes/api');
+const apiPrivateRouter = require('./routes/api-private');
+
+const { connect } = require('./config/database');
+
+connect();
+
+const app = express();
+
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', apiRouter);
+app.use(AuthMiddleware.verifyAuth);
+app.use('/api', apiPrivateRouter);
+
+// Export regular app for local development
+module.exports = app;
+
+// Export serverless app for Netlify
+module.exports.handler = serverless(app);
